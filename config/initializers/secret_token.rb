b16d61e337bf163291bee2460f71fb898183fc6b
Cavin <cstone2003@hotmail.com>
2014-05-09 07:55:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testvideo::Application.config.secret_token = 'a6f3a9620b7d9e8470870c3fa601f4d6cccd04cda8f52d840918b035043834ec4f6375deb57fb3b121e12c8befc115aecd28d5b8fd28b6034ce3af212ad09cb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testvideo::Application.config.session_store :cookie_store, key: '_testvideo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testvideo::Application.config.session_store :active_record_store
